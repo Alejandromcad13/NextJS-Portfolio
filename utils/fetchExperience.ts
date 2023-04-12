7dc14b8c0d2234de448914bde512a3db4ec73676
Bersrk <migueltrader13@gmail.com>
2023-04-11 23:21:25 -0500

    fixed experience type

--- utils/fetchExperience.ts
+++ utils/fetchExperience.ts
@@ -1,4 +1,4 @@
-import { Experience } from "@/typings";
+import { Experiences } from "@/typings";
 
 export const fetchExperience = async () => {
     //use the async fetch function to get the contents of an endpoint
@@ -6,7 +6,7 @@ export const fetchExperience = async () => {
     //we conver the response to readable json format
     const data = await response.json()
     //we type check the response to make sure the values have the right data types
-    const experience:Experience[] = data.experience;
+    const experience:Experiences[] = data.experience;
 
     return experience;
 }
